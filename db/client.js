e3a714bbdbeb024432f6f2139d30ee9104a9473c
fredieposh <maor1234@gmail.com>
2026-01-03 19:44:09 +0200

    Create dev table

---
+++ db/client.js
@@ -0,0 +1,33 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: "postgresql://maorgo92@localhost:5432/inventory_app",
+});
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS inventory (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        product_name VARCHAR ( 255 ),
+        category VARCHAR ( 255 ),
+        price NUMERIC(5, 2),
+        quantity INTEGER
+    );
+
+    INSERT INTO inventory (product_name ,category ,price ,quantity)
+    VALUES
+        ('banana' ,'fruit' ,4.00 ,3),
+        ('milk' ,'dairy' ,2.55 ,10),
+        ('beans' ,'lentils' ,3.15 ,3);
+`;
+
+async function main() {
+    console.log("initiating table..")
+    await client.connect()
+    console.log("connection established...")
+    await client.query(SQL);
+    console.log("table created")
+    await client.end();
+    console.log("done")
+};
+
+main();
