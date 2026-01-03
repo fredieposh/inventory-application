dd77d460be3309ec3b6e2292468221357114643b
fredieposh <maor1234@gmail.com>
2026-01-03 19:07:24 +0200

    Create app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, error => {
+    if (error) {
+        throw error;
+    };
+
+    console.log(`Listening on ${PORT}`);
+});
