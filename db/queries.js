1408140e6ca49ea71e506aeb944cd052fba533fd
fredieposh <maor1234@gmail.com>
2026-01-03 19:44:44 +0200

    Create basic db API

---
+++ db/queries.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: "postgresql://maorgo92@localhost:5432/inventory_app",
+});
+
+exports.getAllInventory = async function () {
+    const { rows } = await pool.query('SELECT * FROM inventory');
+    return rows;
+};
