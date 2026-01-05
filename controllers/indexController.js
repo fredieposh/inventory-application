4f80b2017abc08ba638359108286e758d9a0c6e8
fredieposh <maor1234@gmail.com>
2026-01-05 21:34:58 +0200

    Add product search validations

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,14 @@
 const dbHandler = require('../db/queries');
+const { query, validationResult, matchedData } = require('express-validator');
+
+const productSearchEmptyErr =   'must not be empty'
+const productSearchLengthErr =  'must be between 2 and 50 letters'
+
+const validateProduct = [
+    query('productName').trim()
+        .notEmpty().withMessage(`Product name ${productSearchEmptyErr}`)
+        .isLength({ min: 2, max: 50 }).withMessage(`Product name ${productSearchLengthErr}`),
+];
 
 exports.getAllEntries = async function(req, res) {
     const rows = await dbHandler.getAllInventory()
@@ -8,21 +18,34 @@ exports.getAllEntries = async function(req, res) {
     });
 };
 
-exports.getSearchedProducts = async function(req, res) {
-    if(Object.keys(req.query).length === 0 ) {
-        res.render('searchProduct', {
-            title: 'searcy',
-        });
-        return;        
-    };
-    const {productName} = req.query;
-    const rows = await dbHandler.getProductInfo(productName);
-    const isFound = rows.length > 0 ? true: false;
-    res.render('index', {
-        title: 'Product Search Results',
-        rows,
-        isResults: true,
-        isFound,
-        productName
-    })
-};
+exports.getSearchedProducts = [
+    validateProduct,
+    async function(req, res) {
+        if(Object.keys(req.query).length === 0 ) {
+            res.render('searchProduct', {
+                title: 'Search Product',
+            });
+            return;        
+        };
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).render('searchProduct', {
+                title: 'Search Product',
+                errors: errors.array(),
+            })
+
+            return;
+        };
+        const {productName} = matchedData(req, { locations: ['query'] });
+        const rows = await dbHandler.getProductInfo(productName);
+        const isFound = rows.length > 0 ? true: false;
+        res.render('index', {
+            title: 'Product Search Results',
+            rows,
+            isResults: true,
+            isFound,
+            productName
+        })
+    },
+]
