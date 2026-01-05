cfb732efdbb7262f3b9b0b498ffdde8fa4f429f2
fredieposh <maor1234@gmail.com>
2026-01-05 23:52:28 +0200

    Add Router for the add route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,6 +4,9 @@ const indexController = require('../controllers/indexController.js')
 const indexRouter = Router();
 
 indexRouter.get('/', indexController.getAllEntries);
+
 indexRouter.get('/search', indexController.getSearchedProducts);
+
+indexRouter.get('/add', indexController.getAddProducts);
 
 module.exports = indexRouter;
